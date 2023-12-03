e3e4c89fa0545b1babbc2c4e071f21ee52a42724
Roberto Casadei <roby.casadei@unibo.it>
2023-12-03 15:12:35 +0100

    exercises on graphs

--- code-python/lab08_graphs.py
+++ code-python/lab08_graphs.py
@@ -5,6 +5,14 @@ import random
 import networkx as nx
 from collections import deque
 
+# Some constants
+L_VISITED = 'visited'
+L_VISIT_ORDER = 'visit_order'
+L_ROOT = 'root'
+L_COLOR = 'color'
+L_PARENT = 'parent'
+L_DIST = 'distance'
+
 def char_range(start='a', end='z'):
     return map(chr,list(range(ord(start),ord(end)+1)))
 
@@ -29,6 +37,27 @@ def plot_basic_graph(G, pos = None, layout = nx.random_layout, seed = None):
     fig.set_facecolor("#ededed")
     plt.show()
 
+# TODO: implement this
+def bfv(g, root, f):
+    pass
+
+# TODO: implement this
+def dfv(g, root, f):
+    pass
+
+# TODO: implement this
+# The function should annotate the nodes with their parent and distance from source
+def dijkstra(g, src):
+    # initialization
+    for n in g.nodes:
+        g.nodes[n][L_VISITED] = False
+        g.nodes[n][L_DIST] = float("inf")
+        g.nodes[n][L_PARENT] = ''
+    pass
+
+def shortest_path(g, src, dest):
+    pass
+
 if __name__ == "__main__":
     g = nx.Graph()
     default_node_attributes = {'visited': False, 'parent': None, 'distance': float("inf"), 'label': '', 'color': '#ededed'}
@@ -36,5 +65,13 @@ if __name__ == "__main__":
     g.add_weighted_edges_from([('A','B',1), ('A','F',3), ('B','C',3), ('B','E',5), ('B','F',1), 
                             ('C','D',2), ('D','E',1), ('D','F',6), ('E','F',2)], )
     pos = { 'A': (0,0.5), 'B': (1,1), 'C': (2,1), 'D':(3,0.5), 'E':(2,0), 'F':(1,0) }
+
+    print("*** BFV")
+    bfv(g, 'A', print) # expected print order: ABFCED
+    print("\n*** DFV")
+    dfv(g, 'A', print) # expected print order: ABCDEF
+    print("\n*** Dijkstra")
+    dijkstra(g, 'A') # should annotate the nodes in the graph
+    print(['A', 'B', 'F', 'E', 'D'] == shortest_path(g, 'A', 'D')) # should be True
 
     plot_basic_graph(g, pos)
