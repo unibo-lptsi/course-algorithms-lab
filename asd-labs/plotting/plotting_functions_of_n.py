43b797ea2674ed9954104015d42c66dd9e30c8be
Roberto Casadei <roby.casadei@unibo.it>
2024-10-27 10:24:26 +0100

    some improvements to solution of plotting

--- asd-labs/plotting/plotting_functions_of_n.py
+++ asd-labs/plotting/plotting_functions_of_n.py
@@ -12,21 +12,20 @@ fig, axes = plt.subplots()
 fig.set_tight_layout(True)
 axes.set_xlabel("n")
 axes.set_ylabel("f(n)")
-
 xlimit = 16
 ylimit = 80
+axes.set_xscale("log")  
+axes.set_ylim(0,ylimit)
 
 X = np.linspace(start=1, stop=xlimit, num=100, endpoint=True) # Return evenly spaced numbers over a specified interval.
 print(X)
+
 for f in range(len(functions)):
     #vfun = np.vectorize(functions[f])
     #y = vfun(X)
     y = np.array(list(map(functions[f],X)))
-    print(y)
     #plt.yscale("log")  
-    axes.set_xscale("log")  
-    axes.set_ylim(0,ylimit)
-    print(funs_labels[f])
+    print('\n', funs_labels[f], '\n', np.array((X,y)).T) # shows all pairs (x,y)
     if label_pos[f] != math.nan:
         idx = (np.abs(y - ylimit)).argmin()
         axes.text(X[idx], (ylimit if y[-1] > ylimit else y[-1]) - f*2, funs_labels[f], )
